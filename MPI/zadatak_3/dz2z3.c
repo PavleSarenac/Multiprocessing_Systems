83b26433532e83ba8f8f917aef3e257efed0e874
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2023-12-16 00:13:49 +0100

    dz2z3.c done.

--- MPI/zadatak_3/dz2z3.c
+++ MPI/zadatak_3/dz2z3.c
@@ -4,14 +4,22 @@
 #include <sys/stat.h>
 #include <mpi.h>
 #include <string.h>
+#include <sys/time.h>
 
 #define SOFTENING 1e-9f
 #define MASTER 0
+#define ACCURACY 0.01f
 
 typedef struct
 {
     float x, y, z, vx, vy, vz;
 } Body;
+
+typedef struct Result
+{
+    double executionTime;
+    Body *bodies;
+} Result;
 
 void randomizeBodies(float *data, int n)
 {
@@ -21,7 +29,35 @@ void randomizeBodies(float *data, int n)
     }
 }
 
-void bodyForce(Body *p, float dt, int n, int start, int end)
+void bodyForceSequential(Body *p, float dt, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        float Fx = 0.0f;
+        float Fy = 0.0f;
+        float Fz = 0.0f;
+
+        for (int j = 0; j < n; j++)
+        {
+            float dx = p[j].x - p[i].x;
+            float dy = p[j].y - p[i].y;
+            float dz = p[j].z - p[i].z;
+            float distSqr = dx * dx + dy * dy + dz * dz + SOFTENING;
+            float invDist = 1.0f / sqrtf(distSqr);
+            float invDist3 = invDist * invDist * invDist;
+
+            Fx += dx * invDist3;
+            Fy += dy * invDist3;
+            Fz += dz * invDist3;
+        }
+
+        p[i].vx += dt * Fx;
+        p[i].vy += dt * Fy;
+        p[i].vz += dt * Fz;
+    }
+}
+
+void bodyForceParallel(Body *p, float dt, int n, int start, int end)
 {
     for (int i = start; i < end; i++)
     {
@@ -64,15 +100,63 @@ void saveToCSV(Body *p, int n, int iter, const char *folder)
     fclose(file);
 }
 
-int main(int argc, char **argv)
+void sequentialImplementation(int argc, char **argv, float *initialBuf, Result *sequentialResult)
 {
+    struct timeval startTime, endTime;
+    gettimeofday(&startTime, NULL);
+
+    int nBodies = atoi(argv[1]);
+    int nIters = atoi(argv[2]);
+    const char *folder = argv[3];
+
+    const float dt = 0.01f;
+
+    mkdir(folder, 0700);
+
+    int bytes = nBodies * sizeof(Body);
+    float *buf = (float *)malloc(bytes);
+    memcpy(buf, initialBuf, bytes);
+    float *fakeBuf = (float *)malloc(bytes);
+    Body *p = (Body *)buf;
+
+    randomizeBodies(fakeBuf, 6 * nBodies);
+
+    for (int iter = 0; iter < nIters; iter++)
+    {
+
+        bodyForceSequential(p, dt, nBodies);
+
+        saveToCSV(p, nBodies, iter, folder);
+
+        for (int i = 0; i < nBodies; i++)
+        {
+            p[i].x += p[i].vx * dt;
+            p[i].y += p[i].vy * dt;
+            p[i].z += p[i].vz * dt;
+        }
+    }
+
+    free(fakeBuf);
+
+    gettimeofday(&endTime, NULL);
+    double executionTime = (endTime.tv_sec - startTime.tv_sec) +
+                           (endTime.tv_usec - startTime.tv_usec) / 1e6;
+
+    sequentialResult->bodies = p;
+    sequentialResult->executionTime = executionTime;
+}
+
+void parallelImplementation(int argc, char **argv, float *initialBuf, Result *parallelResult)
+{
+    struct timeval startTime, endTime;
+    gettimeofday(&startTime, NULL);
+
     int nBodies, nIters;
     const char *folder;
 
     const float dt = 0.01f;
 
     int processRank, communicatorSize;
-    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
     MPI_Comm_size(MPI_COMM_WORLD, &communicatorSize);
 
@@ -89,16 +173,18 @@ int main(int argc, char **argv)
 
     int bytes = nBodies * sizeof(Body);
     float *buf = (float *)malloc(bytes);
+    memcpy(buf, initialBuf, bytes);
+    float *fakeBuf = (float *)malloc(bytes);
     float *recvBuf;
     Body *p = (Body *)buf;
 
     if (processRank == MASTER)
     {
-        randomizeBodies(buf, 6 * nBodies);
+        randomizeBodies(fakeBuf, 6 * nBodies);
         recvBuf = (float *)malloc(bytes);
     }
 
-    MPI_Bcast(buf, nBodies * 6, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+    MPI_Bcast(fakeBuf, nBodies * 6, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
 
     int chunkSize = (nBodies + communicatorSize - 1) / communicatorSize;
     int start = processRank * chunkSize;
@@ -121,7 +207,7 @@ int main(int argc, char **argv)
 
     for (int iter = 0; iter < nIters; iter++)
     {
-        bodyForce(p, dt, nBodies, start, end);
+        bodyForceParallel(p, dt, nBodies, start, end);
 
         MPI_Gatherv(buf + start * 6, (end - start) * 6, MPI_FLOAT,
                     recvBuf, recvCounts, displacements, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
@@ -140,12 +226,96 @@ int main(int argc, char **argv)
             }
         }
 
-        MPI_Bcast(buf, nBodies * 6, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+        if (iter < nIters)
+        {
+            MPI_Bcast(buf, nBodies * 6, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+        }
     }
 
-    free(buf);
+    free(fakeBuf);
     if (processRank == MASTER)
+    {
         free(recvBuf);
+
+        gettimeofday(&endTime, NULL);
+        double executionTime = (endTime.tv_sec - startTime.tv_sec) +
+                               (endTime.tv_usec - startTime.tv_usec) / 1e6;
+
+        parallelResult->bodies = p;
+        parallelResult->executionTime = executionTime;
+    }
+}
+
+int areResultsEqual(Result *sequentialResult, Result *parallelResult, int nBodies)
+{
+    Body *seq = sequentialResult->bodies,
+         *par = parallelResult->bodies;
+    for (int i = 0; i < nBodies; i++)
+    {
+        if ((seq[i].x < (par[i].x - ACCURACY)) || (seq[i].x > (par[i].x + ACCURACY)) ||
+            (seq[i].y < (par[i].y - ACCURACY)) || (seq[i].y > (par[i].y + ACCURACY)) ||
+            (seq[i].z < (par[i].z - ACCURACY)) || (seq[i].z > (par[i].z + ACCURACY)) ||
+            (seq[i].vx < (par[i].vx - ACCURACY)) || (seq[i].vx > (par[i].vx + ACCURACY)) ||
+            (seq[i].vy < (par[i].vy - ACCURACY)) || (seq[i].vy > (par[i].vy + ACCURACY)) ||
+            (seq[i].vz < (par[i].vz - ACCURACY)) || (seq[i].vz > (par[i].vz + ACCURACY)))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+    int processRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
+
+    Result *sequentialResult, *parallelResult = malloc(sizeof(Result));
+
+    int nBodies = atoi(argv[1]);
+    int bytes = nBodies * sizeof(Body);
+    float *initialBuf = (float *)malloc(bytes);
+
+    if (processRank == MASTER)
+    {
+        sequentialResult = malloc(sizeof(Result));
+        randomizeBodies(initialBuf, nBodies * 6);
+    }
+
+    MPI_Bcast(initialBuf, nBodies * 6, MPI_FLOAT, MASTER, MPI_COMM_WORLD);
+
+    if (processRank == MASTER)
+    {
+        /*
+         * The main purpose of executing sequential implementation here is to retrieve its results so
+         * that they can be compared with the results of the parallel implementation. The execution time
+         * of the sequential implementation here is not relevant because it is executed inside of the MPI
+         * world, so it is much slower than the actual sequential implementation executed outside of the
+         * MPI world.
+         */
+        sequentialImplementation(argc, argv, initialBuf, sequentialResult);
+        printf("Sequential implementation execution time: %fs\n", sequentialResult->executionTime);
+    }
+
+    parallelImplementation(argc, argv, initialBuf, parallelResult);
+    if (processRank == MASTER)
+    {
+        printf("Parallel implementation execution time: %fs\n", parallelResult->executionTime);
+        if (areResultsEqual(sequentialResult, parallelResult, nBodies))
+            printf("Test PASSED\n");
+        else
+            printf("Test FAILED\n");
+    }
+
+    free(initialBuf);
+    free(parallelResult->bodies);
+    if (processRank == MASTER)
+    {
+        free(sequentialResult->bodies);
+        free(sequentialResult);
+        free(parallelResult);
+    }
     MPI_Finalize();
     return 0;
 }
